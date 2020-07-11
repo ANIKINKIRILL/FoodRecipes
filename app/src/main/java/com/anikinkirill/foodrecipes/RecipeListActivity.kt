c93c2fce8103155de258238f652f2b6ac2758767
ANIKINKIRILL <ky.anikin@mail.ru>
2020-07-11 18:55:32 +0300

    Rename MainActivity

--- app/src/main/java/com/anikinkirill/foodrecipes/RecipeListActivity.kt
+++ app/src/main/java/com/anikinkirill/foodrecipes/RecipeListActivity.kt
@@ -3,10 +3,10 @@ package com.anikinkirill.foodrecipes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RecipeListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_recipe_list)
     }
 }
