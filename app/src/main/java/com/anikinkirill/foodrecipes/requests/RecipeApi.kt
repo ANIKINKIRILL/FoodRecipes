96274b38b779f25426ad95d2a5f404cd6d774eaa
ANIKINKIRILL <ky.anikin@mail.ru>
2020-07-11 21:07:13 +0300

    Create Retfoit interface methods
    This methods are for reqests to the Rest Api

---
+++ app/src/main/java/com/anikinkirill/foodrecipes/requests/RecipeApi.kt
@@ -0,0 +1,24 @@
+package com.anikinkirill.foodrecipes.requests
+
+import com.anikinkirill.foodrecipes.responses.RecipeResponse
+import com.anikinkirill.foodrecipes.responses.RecipeSearchResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RecipeApi {
+
+    @GET("api/search")
+    fun searchRecipe(
+        @Query("key") api_key: String,
+        @Query("q") query: String,
+        @Query("page") page: Int
+    ) : Call<RecipeSearchResponse>
+
+    @GET("api/get")
+    fun getRecipeById(
+        @Query("key") api_key: String,
+        @Query("rId") recipe_id: Int
+    ) : Call<RecipeResponse>
+
+}
