8efa19483238d5d5939291cbf52abb734f207c1e
ANIKINKIRILL <ky.anikin@mail.ru>
2020-07-11 22:22:11 +0300

    Tested

--- app/src/main/java/com/anikinkirill/foodrecipes/RecipeListActivity.kt
+++ app/src/main/java/com/anikinkirill/foodrecipes/RecipeListActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.View
 import com.anikinkirill.foodrecipes.requests.ServiceGenerator
 import com.anikinkirill.foodrecipes.responses.RecipeResponse
+import com.anikinkirill.foodrecipes.responses.RecipeSearchResponse
 import com.anikinkirill.foodrecipes.util.Constants
 import kotlinx.android.synthetic.main.activity_recipe_list.*
 import retrofit2.Call
@@ -35,8 +36,8 @@ class RecipeListActivity : BaseActivity() {
 
     private fun testRecipeResponses() {
         val recipeApi = ServiceGenerator.recipeApi
-        val callRecipeApi = recipeApi.getRecipeById(Constants.API_KEY, 35109)
-        callRecipeApi.enqueue(object: Callback<RecipeResponse> {
+        val callRecipe = recipeApi.getRecipeById(Constants.API_KEY, 35109)
+        callRecipe.enqueue(object: Callback<RecipeResponse> {
             override fun onFailure(call: Call<RecipeResponse>, t: Throwable) {
                 Log.d(TAG, "onFailure: ${t.message}")
             }
@@ -45,6 +46,18 @@ class RecipeListActivity : BaseActivity() {
                 Log.d(TAG, "onResponse: ${response.body()?.recipe?.title}")
             }
         })
+
+        val callRecipeSearch = recipeApi.searchRecipe(Constants.API_KEY, "chicken", 1)
+        callRecipeSearch.enqueue(object: Callback<RecipeSearchResponse> {
+            override fun onFailure(call: Call<RecipeSearchResponse>, t: Throwable) {
+                Log.d(TAG, "onFailure: ${t.message}")
+            }
+
+            override fun onResponse(call: Call<RecipeSearchResponse>, response: Response<RecipeSearchResponse>) {
+                Log.d(TAG, "onResponse: ${response.body()!!.recipes[0].title}")
+            }
+        })
+
     }
 
 }
