ce3998ca9cb37c77c1ee470f75c7cda4dc13de06
ANIKINKIRILL <ky.anikin@mail.ru>
2020-07-11 21:09:26 +0300

    Change RecipeResponse

--- app/src/main/java/com/anikinkirill/foodrecipes/responses/RecipeResponse.kt
+++ app/src/main/java/com/anikinkirill/foodrecipes/responses/RecipeResponse.kt
@@ -4,10 +4,10 @@ import com.anikinkirill.foodrecipes.models.Recipe
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-class RecipeResponse {
+data class RecipeResponse(
 
     @SerializedName("recipe")
     @Expose
-    lateinit var recipe: Recipe
+    private val recipe: Recipe
 
-}
+)
