c402226bb7470dd7b7e7ba46a1972fe9ec4fbd9a
ANIKINKIRILL <ky.anikin@mail.ru>
2020-07-11 19:25:56 +0300

    Create Constants class

---
+++ app/src/main/java/com/anikinkirill/foodrecipes/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.anikinkirill.foodrecipes.util
+
+class Constants {
+
+    companion object {
+
+        const val API_KEY = "" // this is blanked, because of the new api "https://recipesapi.herokuapp.com/"
+        const val BASE_URL = "https://recipesapi.herokuapp.com"
+
+    }
+
+}
