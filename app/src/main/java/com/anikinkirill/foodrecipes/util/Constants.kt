159898b86a8f39f1ac868e8f1d4284c8aeda56c9
ANIKINKIRILL <ky.anikin@mail.ru>
2020-07-12 20:14:20 +0300

    Add categories

--- app/src/main/java/com/anikinkirill/foodrecipes/util/Constants.kt
+++ app/src/main/java/com/anikinkirill/foodrecipes/util/Constants.kt
@@ -7,6 +7,29 @@ class Constants {
         const val API_KEY = "" // this is blanked, because of the new api "https://recipesapi.herokuapp.com/"
         const val BASE_URL = "https://recipesapi.herokuapp.com"
         const val NETWORK_TIMEOUT = 3000    // 3 seconds
+
+        val DEFAULT_SEARCH_CATEGORIES = arrayOf(
+            "Barbeque",
+            "Breakfast",
+            "Chicken",
+            "Beef",
+            "Brunch",
+            "Dinner",
+            "Wine",
+            "Italian"
+        )
+
+        val DEFAULT_SEARCH_CATEGORY_IMAGES = arrayOf(
+            "barbeque",
+            "breakfast",
+            "chicken",
+            "beef",
+            "brunch",
+            "dinner",
+            "wine",
+            "italian"
+        )
+
     }
 
 }
