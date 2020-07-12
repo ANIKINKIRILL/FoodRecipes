8f6eecfeae25dee72c2fc52e85090b10a02dd785
ANIKINKIRILL <ky.anikin@mail.ru>
2020-07-12 16:42:51 +0300

    Create LoadingViewHolder class

---
+++ app/src/main/java/com/anikinkirill/foodrecipes/adapters/LoadingViewHolder.kt
@@ -0,0 +1,8 @@
+package com.anikinkirill.foodrecipes.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class LoadingViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+}
