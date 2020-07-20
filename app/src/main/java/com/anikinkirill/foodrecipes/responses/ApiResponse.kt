a44586665b50767c4a8b279ed36d57ad7a5509b5
ANIKINKIRILL <ky.anikin@mail.ru>
2020-07-20 18:10:36 +0300

    Create generic class for handling retrofit responses

---
+++ app/src/main/java/com/anikinkirill/foodrecipes/responses/ApiResponse.kt
@@ -0,0 +1,42 @@
+package com.anikinkirill.foodrecipes.responses
+
+import retrofit2.Response
+import java.io.IOException
+
+sealed class ApiResponse<T> {
+
+    fun create(error: Throwable) : ApiResponse<T> {
+        return ApiErrorResponse((if (error.message != "") error.message else "Unknown error\\nCheck network connection").toString())
+    }
+
+    fun create(response: Response<T>) : ApiResponse<T> {
+        if(response.isSuccessful){
+            val body: T? = response.body()
+            return if(body == null || response.code() == 204){
+                ApiEmptyResponse()
+            }else{
+                ApiSuccessResponse(body)
+            }
+        }else{
+            val errorMessage: String
+            errorMessage = try {
+                response.errorBody().toString()
+            }catch (ex: IOException) {
+                ex.printStackTrace()
+                response.message()
+            }
+            return ApiErrorResponse(errorMessage)
+        }
+    }
+
+    data class ApiSuccessResponse<T>(
+        private var body: T
+    ) : ApiResponse<T>()
+
+    data class ApiErrorResponse<T>(
+        private var errorMessage: String
+    ) : ApiResponse<T>()
+
+    class ApiEmptyResponse<T> : ApiResponse<T>()
+
+}
