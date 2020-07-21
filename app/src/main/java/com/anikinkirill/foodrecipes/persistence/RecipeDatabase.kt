b9adfcaf273fec4557e643edf2606835ae09ca74
ANIKINKIRILL <ky.anikin@mail.ru>
2020-07-21 21:29:23 +0300

    Room Database

---
+++ app/src/main/java/com/anikinkirill/foodrecipes/persistence/RecipeDatabase.kt
@@ -0,0 +1,33 @@
+package com.anikinkirill.foodrecipes.persistence
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.anikinkirill.foodrecipes.models.Recipe
+
+@Database(entities = [Recipe::class], version = 1)
+abstract class RecipeDatabase : RoomDatabase() {
+
+    companion object {
+        @Volatile
+        private var INSTANCE: RecipeDatabase? = null
+
+        fun getDatabase(context: Context): RecipeDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    RecipeDatabase::class.java,
+                    "recipe_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+
+}
