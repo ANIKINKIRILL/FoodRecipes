58e7e5af8b18d59f01e6a670b2ab268e60ec0c0a
ANIKINKIRILL <ky.anikin@mail.ru>
2020-07-24 20:18:29 +0300

    Refreshing the Cache

--- app/src/main/java/com/anikinkirill/foodrecipes/repositories/RecipeRepository.kt
+++ app/src/main/java/com/anikinkirill/foodrecipes/repositories/RecipeRepository.kt
@@ -1,6 +1,7 @@
 package com.anikinkirill.foodrecipes.repositories
 
 import android.content.Context
+import android.util.Log
 import androidx.lifecycle.LiveData
 import com.anikinkirill.foodrecipes.models.Recipe
 import com.anikinkirill.foodrecipes.persistence.RecipeDao
@@ -18,6 +19,9 @@ class RecipeRepository private constructor(private val context: Context) {
     private val recipeDao: RecipeDao = RecipeDatabase.getDatabase(context).recipeDao()
 
     companion object {
+
+        private const val TAG = "RecipeRepository"
+
         var instance: RecipeRepository? = null
         fun getInstance(context: Context) : RecipeRepository {
             if(instance == null) {
@@ -30,7 +34,24 @@ class RecipeRepository private constructor(private val context: Context) {
     fun searchRecipesApi(query: String, page: Int) : LiveData<Resource<List<Recipe>>> {
         return object : NetworkBoundResource<List<Recipe>, RecipeSearchResponse>(AppExecutors.instance){
             override fun saveCallResult(item: RecipeSearchResponse) {
-                TODO("Not yet implemented")
+                var recipes = arrayOfNulls<Recipe>(item.recipes.size)
+                for(i in item.recipes.indices) {
+                    recipes[i] = item.recipes[i]
+                }
+                for((index, rowId) in recipeDao.insertRecipes(*recipes as Array<out Recipe>).withIndex()) {
+                    if(rowId == -1L) {
+                        // means that recipe already in the cache
+                        // in this case we should not save ingredients or timestamp
+                        Log.d(TAG, "saveCallResult: CONFLICT. The recipe already in the cache  ")
+                        recipeDao.updateRecipe(
+                            recipes[index].recipe_id!!,
+                            recipes[index].title!!,
+                            recipes[index].publisher!!,
+                            recipes[index].image_url!!,
+                            recipes[index].social_rank!!
+                        )
+                    }
+                }
             }
 
             override fun shouldFetch(data: List<Recipe>?): Boolean {
