fa451ee15927f45b17ae8005a96b3c85acc8f14f
ANIKINKIRILL <ky.anikin@mail.ru>
2020-07-24 17:06:32 +0300

    Repository Setup

--- app/src/main/java/com/anikinkirill/foodrecipes/repositories/RecipeRepository.kt
+++ app/src/main/java/com/anikinkirill/foodrecipes/repositories/RecipeRepository.kt
@@ -1,89 +1,48 @@
 package com.anikinkirill.foodrecipes.repositories
 
+import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
 import com.anikinkirill.foodrecipes.models.Recipe
-import com.anikinkirill.foodrecipes.requests.RecipeApiClient
+import com.anikinkirill.foodrecipes.persistence.RecipeDao
+import com.anikinkirill.foodrecipes.persistence.RecipeDatabase
+import com.anikinkirill.foodrecipes.responses.ApiResponse
+import com.anikinkirill.foodrecipes.responses.RecipeSearchResponse
+import com.anikinkirill.foodrecipes.util.AppExecutors
+import com.anikinkirill.foodrecipes.util.NetworkBoundResource
+import com.anikinkirill.foodrecipes.util.Resource
 
-class RecipeRepository private constructor() {
+class RecipeRepository private constructor(private val context: Context) {
 
-    private val apiClient = RecipeApiClient.instance
-    private var mQuery: String? = null
-    private var mPageNumber: Int? = null
-
-    private var isQueryExhausted = MutableLiveData<Boolean>()
-    private var mRecipes = MediatorLiveData<List<Recipe>>()
+    private val recipeDao: RecipeDao = RecipeDatabase.getDatabase(context).recipeDao()
 
     companion object {
-        val instance: RecipeRepository by lazy {
-            RecipeRepository()
-        }
-    }
-
-    init {
-        initMediators()
-    }
-
-    private fun initMediators() {
-        val source = apiClient.getRecipesList()
-        mRecipes.addSource(source) {
-            if(it != null) {
-                mRecipes.value = it
-                doneQuery(it)
-            }else{
-                // search database cache
-                doneQuery(null)
+        var instance: RecipeRepository? = null
+        fun getInstance(context: Context) : RecipeRepository {
+            if(instance == null) {
+                instance = RecipeRepository(context)
             }
+            return instance as RecipeRepository
         }
     }
 
-    private fun doneQuery(list: List<Recipe>?){
-        if(list != null) {
-            if (list.size % 30 != 0) {
-                isQueryExhausted.value = true
+    fun searchRecipesApi(query: String, page: Int) : LiveData<Resource<List<Recipe>>> {
+        return object : NetworkBoundResource<List<Recipe>, RecipeSearchResponse>(AppExecutors.instance){
+            override fun saveCallResult(item: RecipeSearchResponse) {
+                TODO("Not yet implemented")
             }
-            if(list.isEmpty()){
-               isQueryExhausted.value = true
+
+            override fun shouldFetch(data: List<Recipe>?): Boolean {
+                TODO("Not yet implemented")
             }
-        }else{
-            isQueryExhausted.value = true
-        }
+
+            override fun loadFromDb(): LiveData<List<Recipe>> {
+                return recipeDao.searchRecipes(query, page)
+            }
+
+            override fun createCall(): LiveData<ApiResponse<RecipeSearchResponse>> {
+                TODO("Not yet implemented")
+            }
+        }.asLiveData()
     }
-
-    fun isQueryExhausted() = isQueryExhausted
-
-    fun getRecipesList() : LiveData<List<Recipe>> {
-        return mRecipes
-    }
-
-    fun searchRecipesApi(query: String, page: Int) {
-        var pageNumber = page
-        if(pageNumber == 0){
-            pageNumber = 1
-        }
-        mQuery = query
-        mPageNumber = pageNumber
-        isQueryExhausted.value = false
-        apiClient.searchRecipesApi(query, pageNumber)
-    }
-
-    fun searchNextPage() {
-        searchRecipesApi(mQuery!!, mPageNumber!! + 1)
-    }
-
-    fun cancelRequest() {
-        apiClient.cancelRequest()
-    }
-
-    fun getRecipeById(recipeId: String) {
-        apiClient.getRecipeById(recipeId)
-    }
-
-    fun getRecipe() : LiveData<Recipe> {
-        return apiClient.getRecipe()
-    }
-
-    fun getNetworkTimeout() : LiveData<Boolean> = apiClient.getNetworkTimeout()
 
 }
